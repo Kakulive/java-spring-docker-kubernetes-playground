122f2f62e61ee17090d25b75beb61895c3f57ed5
Kaku <79337205+Kakulive@users.noreply.github.com>
2022-10-10 17:14:53 +0200

    spring project initialized

--- src/main/java/com/njoyer/jsdk/HelloWorldController.java
+++ src/main/java/com/njoyer/jsdk/HelloWorldController.java
@@ -1,2 +1,13 @@
-package com.njoyer.jsdk;public class HelloWorldController {
+package com.njoyer.jsdk;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(value = "/hello")
+    public String sayHello() {
+        return "Hello, welcome to my Micro Service";
+    }
 }
---
+++ src/main/java/com/njoyer/jsdk/JsdkApplication.java
@@ -0,0 +1,13 @@
+package com.njoyer.jsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsdkApplication.class, args);
+	}
+
+}
