b49a52106915e762f2cd23eb2652ea50632a2488
Kaku <79337205+Kakulive@users.noreply.github.com>
2022-10-14 16:20:54 +0200

    Properties config added to accounts

--- accounts/src/main/java/com/njoyer/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/njoyer/accounts/controller/AccountsController.java
@@ -1,9 +1,15 @@
 package com.njoyer.accounts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.njoyer.accounts.config.AccountsServiceConfig;
 import com.njoyer.accounts.model.Accounts;
 import com.njoyer.accounts.model.Customer;
+import com.njoyer.accounts.model.Properties;
 import com.njoyer.accounts.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +20,21 @@ public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
+    @Autowired
+    AccountsServiceConfig accountsConfig;
+
     @PostMapping("/myAccount")
     public Accounts getAccountDetails(@RequestBody Customer customer) {
-        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 
-        if (accounts != null) {
-            return accounts;
-        } else {
-            return null;
-        }
+        return accountsRepository.findByCustomerId(customer.getCustomerId());
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        return ow.writeValueAsString(properties);
     }
 
 }
